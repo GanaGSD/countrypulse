52555a1015241e0e9eb1ac93a65290d810d39098
Gana <jgantsog6@gmail.com>
2025-07-17 13:58:45 -0700

    Fix on Deployment
    Destinations page has been updated

--- src/app/destinations/page.tsx
+++ src/app/destinations/page.tsx
@@ -1,13 +1,7 @@
-import Hero from '@/components/home/Hero';
 import { TimelineDemo } from '@/components/home/Timeline';
-import { TimelineDemo2 } from '@/components/home/Timeline2';
 import { InfiniteMovingCardsDemo } from '@/components/home/MovingCards';
 
 export default function Home() {
-  const narratives = [
-    `Mongolia is a land where the ancient and modern coexist, where nomadic traditions that have endured for centuries continue alongside a rapidly developing society.`,
-    `From the rolling steppe to the towering mountains, from the scorching desert to the cooling forests and lakes, Mongolia's varied landscapes provide the backdrop for adventure, cultural immersion, and unforgettable memories.`
-  ];
   
   return (
     <div>
